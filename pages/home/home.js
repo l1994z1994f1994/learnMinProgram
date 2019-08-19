e629842f816d9fddd50061c8514ba4b70ac6711b
l1994z1994f1994 <974919773@qq.com>
2019-08-19 17:53:33 +0800

    小程序事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,59 @@
 // home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list:["上衣","裤子","鞋子"]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleEvent(){
+    console.log("绑定事件");
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTouchStart(){       //手指开始触摸
+    console.log("handleTouchStart")
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleTouchMove() {       //手指开始移动
+    console.log("handleTouchMove")
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleTouchEnd() {         //手指离开
+    console.log("handleTouchEnd")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleTap() {              //触摸事件，快速
+    console.log("handleTap")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  handleLongpress(){        //长按事件，时间超过350ms
+    console.log("handleLongpress")
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  // 事件对象的分析
+  handleOuter(event){
+    console.log(event);
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  handleInter(event) {
+    console.log(event);
+  },
+  // 事件的传递参数
+  handleItem(event){
+    console.log(event);
+    // const dataset = event.target.dataset;
+    const dataset = event.currentTarget.dataset;
+    const index = dataset.index;
+    const item = dataset.item;
+    console.log(index, item);
+  },
+  // 事件冒泡与事件捕获
+  handleOne(){
+    console.log("one");
+  },
+  handleTwo() {
+    console.log("two");
+  },
+  handleThree() {
+    console.log("three");
+  },
+  handleOneView(){
+    console.log("handleOneView");
+  },
+  handleTwoView() {
+    console.log("handleTwoView");
+  },
+  handleThreeView() {
+    console.log("handleThreeView");
+  },
 })
